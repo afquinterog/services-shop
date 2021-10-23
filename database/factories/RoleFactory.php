53a929c9a909c753d8c83caa414b0c46781e5740
Andres Quintero <afquinterog@users.noreply.github.com>
2021-10-22 22:19:45 -0500

    add factories

---
+++ database/factories/RoleFactory.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Role;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class RoleFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Role::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+        ];
+    }
+
+    /**
+     * Create an admin role
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function admin()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'type' => 'admin'
+            ];
+        });
+    }
+
+    /**
+     * Create an admin role
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function owner()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'type' => 'owner'
+            ];
+        });
+    }
+}
