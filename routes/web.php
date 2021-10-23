c41cbcc4b6b6177082159aa9fe129a3a5a873497
Andres Quintero <afquinterog@users.noreply.github.com>
2021-10-22 22:21:51 -0500

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\ShopAdminController;
+use App\Http\Controllers\ShopController;
+use App\Models\Order;
+use App\Notifications\ProductOrderedNotification;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +18,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [ShopController::class, 'index'])->middleware('link.domain.company');
+Route::get('/products/{products:slug}', [ShopController::class, 'details'])->middleware('link.domain.company')->name('product-details');;
+Route::resource('products', ShopController::class)->middleware('link.domain.company');
+
+
+
+Route::get('/web', function () {
+    return view('web');
+});
+
+Route::get('/email', function () {
+    Notification::route('mail', 'rocoutp@gmail.com')
+        ->notify(new ProductOrderedNotification(Order::findOrFail(9)));
+    return "hi";
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/manage', [ShopAdminController::class, 'index'])->middleware(['auth'])->name('manage');
+//Route::get('/manage', function () {
+//    return view('dashboard');
+//})->middleware(['auth'])->name('manage');
 
 require __DIR__.'/auth.php';
