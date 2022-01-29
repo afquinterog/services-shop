ed42b9dc1c69f6b112771b4d886431cc31e831cb
Andres Quintero <afquinterog@users.noreply.github.com>
2022-01-29 05:22:34 -0500

    Remove unused spaces

--- resources/views/livewire/manage-products/edit-product-rating-list.blade.php
+++ resources/views/livewire/manage-products/edit-product-rating-list.blade.php
@@ -22,7 +22,7 @@
         @forelse ($product->ratings()->get() as $rating)
             <tr>
                 <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                    
+
                     <x-rating :rate="$rating->rating" />
                 </td>
                 <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
