fbc052b3ed99e3f07a9e47da003d021e390506ff
Andres Quintero <afquinterog@users.noreply.github.com>
2022-01-29 05:22:47 -0500

    Group session call into one function

--- app/Http/Middleware/LinkGuestRequestToCompany.php
+++ app/Http/Middleware/LinkGuestRequestToCompany.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use App\Models\Domain;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
@@ -23,10 +24,14 @@ class LinkGuestRequestToCompany
         $domain = Domain::where('domain', $domain)->first();
         abort_if(! $domain, 403);
 
-        session(['company_id' => $domain->company_id]);
-        session(['company_name' => $domain->company->name]);
-        session(['company' => $domain->company]);
-        session(['theme' => $domain->company->theme]);
+        session([
+            'company_id' => $domain->company_id,
+            'company_name' => $domain->company->name,
+            'company' => $domain->company,
+            'theme' => $domain->company->theme
+        ]);
+
+        App::setLocale($domain->locale);
         return $next($request);
     }
 }
