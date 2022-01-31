e34a13c0e05d1c47a3d091f2a81e77773c5f9a9a
Andres Quintero <afquinterog@users.noreply.github.com>
2022-01-30 22:02:26 -0500

    Add seeder command

---
+++ app/Console/Commands/CompanySeed.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Category;
+use App\Models\Company;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class CompanySeed extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'company:seed {company?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a company and seed some demo data';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $company = $this->argument('company');
+
+        if ($company) {
+            $this->deleteCompany($company);
+        } else {
+            $this->createCompany();
+        }
+
+        return 0;
+    }
+
+    public function createCompany()
+    {
+        $user = User::factory()->create();
+
+        Auth::login($user);
+
+        $company = Company::factory()->create([
+            'user_id' => $user->id
+        ]);
+
+        Category::factory()
+            ->has(Product::factory()->count(30))
+            ->create([
+                'company_id' => $company->id
+            ]);
+
+        Auth::logout();
+    }
+
+    public function deleteCompany($company)
+    {
+        $company = Company::find($company);
+
+        $company->categories()->get()->each( function ($category) {
+            DB::table('category_product')->where('category_id', $category->id)->delete();
+        });
+
+        $company->categories()->delete();
+        $company->products()->delete();
+        $company->delete();
+    }
+}
